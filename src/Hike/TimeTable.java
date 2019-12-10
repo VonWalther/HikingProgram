faa8faa6fe7331cb97266579bc14ba232aecdcef
DracoDragonSauce <gabrielsilvey@live.com>
2019-12-10 09:07:50 -0600

    Created a class for a time table 12/10/19

--- src/Hike/TimeTable.java
+++ src/Hike/TimeTable.java
@@ -1,28 +1,4 @@
 package Hike;
 
-
-import java.util.HashMap;
-
-
-
 public class TimeTable {
-
-    private HashMap timeMap;
-
-    public TimeTable() {
-        HashMap<Character , Integer> timeMap = new HashMap<>();
-        timeMap.put('T' , 3);
-        timeMap.put('W' , 8);
-        timeMap.put('O' , 12);
-        timeMap.put('S' , 20);
-        this.timeMap = timeMap;
-    }
-
-    public int getTime(char terrain) {
-        if (timeMap.containsKey(Character.toUpperCase(terrain))) {
-            return (int) timeMap.get(Character.toUpperCase(terrain));
-        } else {
-            return 100;
-        }
-    }
 }
