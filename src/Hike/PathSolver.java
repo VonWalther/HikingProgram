c60829f6c68d8ba99cd1289b51edc2d9b56eea0d
VonWalther <carl.walther@sbcglobal.net>
2019-12-11 09:32:33 -0600

    Started Path Solver Decided to go with a custom Node for Priority Queue

--- src/Hike/PathSolver.java
+++ src/Hike/PathSolver.java
@@ -1,4 +1,29 @@
 package Hike;
 
+import java.util.PriorityQueue;
+
+//Created by Carl F. Walther
+//For the Hikeing Problem 12/11/19
 public class PathSolver {
+
+    //Data Members
+    private PriorityQueue<Integer> nextNode;
+    private TerrainMap currentMap;
+    private int[][] timeMap = new int[5][5];
+
+
+
+    //Constructors
+    public PathSolver(TerrainMap currentMap){
+        this.currentMap = currentMap;
+        this.nextNode = new PriorityQueue<Integer>();
+        //Remove this next part after testing.
+        nextNode.offer(4);
+        nextNode.offer(6);
+    }
+
+    public int getFastestTime(){
+
+        return(7);
+    }
 }
