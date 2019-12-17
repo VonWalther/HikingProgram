12a693a3336297a19910814b183624360b0e178a
73134 <54319703+73134@users.noreply.github.com>
2019-12-17 08:31:31 -0600

    color coded map, added ability to read file
    will now directly read in from input file, and display the given information (start/end points, terrain types, file name, etc)

--- src/Hike/Controller.java
+++ src/Hike/Controller.java
@@ -3,21 +3,24 @@ package Hike;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
+
+    private static ArrayList<TerrainMap> allMaps;
+    private static int mapIndex = 0;
 
     @FXML
     private Button prevMapButton;
@@ -29,16 +32,31 @@ public class Controller {
     private Button loadNew;
 
     @FXML
+    private TextField endPointBox;
+
+    @FXML
     private GridPane grid;
 
     @FXML
+    private TextField startPointBox;
+
+    @FXML
+    private TextField bestTimeBox;
+
+    @FXML
     void loadNext(ActionEvent event) {
-        //load next map
+        if (!(mapIndex >= allMaps.size() - 1)){
+            mapIndex++;
+            loadCurrentMap();
+        }
     }
 
     @FXML
     void loadPrev(ActionEvent event) {
-        //load last map
+        if (!(mapIndex <= 0)){
+            mapIndex--;
+            loadCurrentMap();
+        }
     }
 
     @FXML
@@ -56,28 +74,90 @@ public class Controller {
         currentStage.close();
     }
 
+    //FXML version of function, used when window is initially drawn
     @FXML
     void loadCurrentMap(MouseEvent event){
         //fill in map here
         grid.setGridLinesVisible(true);
 
-        //dummy entry
         char[][] array = new char[5][5];
+        TerrainMap currentMap = allMaps.get(mapIndex);
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
-                array[i][j] = 'T';
+                array[i][j] = currentMap.getMapPosition(i,j);
             }
         }
-        //end dummy entry
+
+        startPointBox.setText("( " + currentMap.getStartingXPoint() + " , " + currentMap.getStartingYPoint() + " )");
+        endPointBox.setText("( " + currentMap.getEndingXPoint() + " , " + currentMap.getEndingYPoint() + " )");
+        bestTimeBox.setText("-----");
 
         for (int x = 0; x < array.length; x++){
             for (int y = 0; y < array[x].length; y++){
                 Label label = new Label(Character.toString(array[x][y]));
                 StackPane sp = new StackPane();
+                switch (array[x][y]){
+                    case 'T':
+                        sp.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'W':
+                        sp.setBackground(new Background(new BackgroundFill(Color.CYAN, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'O':
+                        sp.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'S':
+                        sp.setBackground(new Background(new BackgroundFill(Color.LIGHTCORAL, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                }
                 sp.getChildren().add(label);
                 grid.add(sp, x, y);
             }
         }
     }
 
+    //used whenever loadNext() or loadPrev() event is triggered
+    void loadCurrentMap(){
+        //fill in map here
+        grid.setGridLinesVisible(true);
+
+        char[][] array = new char[5][5];
+        TerrainMap currentMap = allMaps.get(mapIndex);
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                array[i][j] = currentMap.getMapPosition(i,j);
+            }
+        }
+
+        startPointBox.setText("( " + currentMap.getStartingXPoint() + " , " + currentMap.getStartingYPoint() + " )");
+        endPointBox.setText("( " + currentMap.getEndingXPoint() + " , " + currentMap.getEndingYPoint() + " )");
+        bestTimeBox.setText("-----");
+
+        for (int x = 0; x < array.length; x++){
+            for (int y = 0; y < array[x].length; y++){
+                Label label = new Label(Character.toString(array[x][y]));
+                StackPane sp = new StackPane();
+                switch (array[x][y]){
+                    case 'T':
+                        sp.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'W':
+                        sp.setBackground(new Background(new BackgroundFill(Color.CYAN, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'O':
+                        sp.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                    case 'S':
+                        sp.setBackground(new Background(new BackgroundFill(Color.LIGHTCORAL, CornerRadii.EMPTY, Insets.EMPTY)));
+                        break;
+                }
+                sp.getChildren().add(label);
+                grid.add(sp, x, y);
+            }
+        }
+    }
+
+    public static void setAllMaps(ArrayList<TerrainMap> maps) {
+        allMaps = maps;
+    }
 }
